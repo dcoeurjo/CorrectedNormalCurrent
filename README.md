ac70bdccd936d8ead8294dda3608426472fd4a83
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2019-12-22 13:29:24 +0100

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # polyscope-dgtal
+
+This repository is an integration example between [polyscope](http://polyscope.run) and [DGtal](dgtal.org).
+
+To use this project, just clone it (with submodule):
+
+```
+git clone --recursive https://github.com/dcoeurjo/polyscope-dgtal.git
+```
+
+
+
+# Authors
+
+* [David Coeurjolly](http://perso.liris.cnrs.fr/david.coeurjolly) (CNRS)
--- simpleTest.cpp
+++ simpleTest.cpp
@@ -3,16 +3,59 @@
 #include <DGtal/helpers/StdDefs.h>
 #include <DGtal/helpers/Shortcuts.h>
 #include <DGtal/helpers/ShortcutsGeometry.h>
-#include "deps/polyscope/polyscope.h"
+#include "polyscope/polyscope.h"
+#include "polyscope/point_cloud.h"
+#include "polyscope/surface_mesh.h"
+
 
 using namespace DGtal;
 using namespace Z3i;
 
+// Using standard 3D digital space.
+typedef Shortcuts<Z3i::KSpace>         SH3;
+typedef ShortcutsGeometry<Z3i::KSpace> SHG3;
+
 int main()
 {
   polyscope::init();
+  
+  auto params = SH3::defaultParameters() | SHG3::defaultParameters() |  SHG3::parametersGeometryEstimation();
+  auto h=0.5;
+  params( "polynomial", "goursat" )( "gridstep", h );
+  auto implicit_shape  = SH3::makeImplicitShape3D  ( params );
+  auto digitized_shape = SH3::makeDigitizedImplicitShape3D( implicit_shape, params );
+  auto K               = SH3::getKSpace( params );
+  auto binary_image    = SH3::makeBinaryImage( digitized_shape, params );
+  auto surface         = SH3::makeDigitalSurface( binary_image, K, params );
+  auto embedder        = SH3::getCellEmbedder( K );
+  SH3::Cell2Index c2i;
+  auto surfels         = SH3::getSurfelRange( surface, params );
+  auto primalSurface   = SH3::makePrimalPolygonalSurface(c2i, surface);
+  
+  polyscope::registerPointCloud("Pointels", primalSurface->positions());
+  
+  std::vector<std::vector<unsigned long>> faces;
+  for(auto &face: primalSurface->allFaces())
+    faces.push_back(primalSurface->verticesAroundFace( face ));
+  
+  auto digsurf = polyscope::registerSurfaceMesh("Primal surface", primalSurface->positions(), faces);
+  digsurf->edgeWidth=1.0;
+  digsurf->edgeColor={1.,1.,1.};
+  
+  //Computing some differential quantities
+  params("r-radius", 5.0);
+  auto normals   = SHG3::getCTrivialNormalVectors(surface, surfels, params);
+  auto normalsII = SHG3::getIINormalVectors(binary_image, surfels, params);
+  auto Mcurv     = SHG3::getIIMeanCurvatures(binary_image, surfels, params);
+  auto Gcurv     = SHG3::getIIGaussianCurvatures(binary_image, surfels, params);
 
-
+  //Attaching quantities
+  digsurf->addFaceVectorQuantity("CTrivial Normal vectors", normals);
+  digsurf->addFaceVectorQuantity("II Normal vectors", normals);
+  digsurf->addFaceScalarQuantity("II Mean curvature", Mcurv);
+  digsurf->addFaceScalarQuantity("II Gaussian curvature", Gcurv);
+  
+  
   polyscope::show();
   return 0;
 }
