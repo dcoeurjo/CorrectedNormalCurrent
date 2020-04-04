7687b98da9a2f7e093076a9598fc63e1ca8dca13
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-04-04 13:40:09 +0200

    minor edit

---
+++ RusinkiewiczCurvatureFormula.h
@@ -0,0 +1,370 @@
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+#pragma once
+
+/**
+ * @file RusinkiewiczCurvatureFormula.h
+ * @author Jacques-Olivier Lachaud (\c jacques-olivier.lachaud@univ-savoie.fr )
+ * Laboratory of Mathematics (CNRS, UMR 5807), University of Savoie, France
+ *
+ * @date 2020/01/01
+ *
+ * Header file for module RusinkiewiczCurvatureFormula.cpp
+ *
+ * This file is part of the DGtal library.
+ */
+
+#if defined(RusinkiewiczCurvatureFormula_RECURSES)
+#error Recursive header files inclusion detected in RusinkiewiczCurvatureFormula.h
+#else // defined(RusinkiewiczCurvatureFormula_RECURSES)
+/** Prevents recursive inclusion of headers. */
+#define RusinkiewiczCurvatureFormula_RECURSES
+
+#if !defined RusinkiewiczCurvatureFormula_h
+/** Prevents repeated inclusion of headers. */
+#define RusinkiewiczCurvatureFormula_h
+
+//////////////////////////////////////////////////////////////////////////////
+// Inclusions
+#include <iostream>
+#include <map>
+#include "DGtal/base/Common.h"
+#include "DGtal/math/linalg/SimpleMatrix.h"
+#include "DGtal/math/linalg/EigenDecomposition.h"
+
+//////////////////////////////////////////////////////////////////////////////
+
+namespace DGtal
+{
+
+  /////////////////////////////////////////////////////////////////////////////
+  // class RusinkiewiczCurvatureFormula
+  /**
+     Description of class 'RusinkiewiczCurvatureFormula' <p> \brief
+     Aim: A helper class that provides static methods to compute
+     Rusinkiewicz formulas of curvatures.
+
+     @tparam TRealPoint any model of 3D RealPoint.
+     @tparam TRealVector any model of 3D RealVector.
+  */
+  template < typename TRealPoint, typename TRealVector >
+  struct RusinkiewiczCurvatureFormula
+  {
+    typedef TRealPoint                     RealPoint;
+    typedef TRealVector                    RealVector;
+    typedef typename RealVector::Component Scalar;
+    typedef std::vector< RealPoint >       RealPoints;
+    typedef std::vector< RealVector >      RealVectors;
+    typedef SimpleMatrix< Scalar, 3, 3 >   RealTensor;
+    typedef SimpleMatrix< Scalar, 2, 2 >   RealTensor2D;
+    typedef typename RealTensor2D::ColumnVector ColumnVector2D;
+    typedef typename RealTensor2D::RowVector    RowVector2D;
+    typedef std::size_t                    Index;
+    static const Dimension dimension = RealPoint::dimension;
+    typedef SimpleMatrix< Scalar, 6, 3 >   LSMatrix;
+    typedef typename LSMatrix::ColumnVector LSColumnVector;
+
+    //-------------------------------------------------------------------------
+  public:
+    /// @name Formulas for curvature
+    /// @{
+
+    /// Computes a local orthonormal basis on triangle abc.
+    /// @param a any point
+    /// @param b any point
+    /// @param c any point
+    /// @return a pair (u,v) of unit orthogonal vector lying in the plane abc.
+    static
+    std::pair< RealVector, RealVector > basis
+    ( const RealPoint& a, const RealPoint& b, const RealPoint& c )
+    {
+      RealVector u = ( b - a ).getNormalized();
+      RealVector v = normal( a, b, c ).crossProduct( u );
+      return std::make_pair( u, v );
+    }    
+
+    /// Computes a local orthonormal basis on face \a x.
+    /// @param x the (ccw ordered) points forming the vertices of a polygonal face.
+    /// @return a pair (u,v) of unit orthogonal vector lying in the plane of the face.
+    static
+    std::pair< RealVector, RealVector > basis
+    ( const RealPoints& x )
+    {
+      if ( x.size() == 3 ) return basis( x[ 0 ], x[ 1 ], x[ 2 ] );
+      else                 return basis( barycenter( x ), x[ 0 ], x[ 1 ] );
+    }
+    
+    
+    /// Computes the local second fundamental form on triangle abc
+    /// given normal vectors \a ua, \a \ub, \a uc at its vertices.
+    ///
+    /// @param a any point
+    /// @param b any point
+    /// @param c any point
+    /// @param ua the corrected normal vector at point a
+    /// @param ub the corrected normal vector at point b
+    /// @param uc the corrected normal vector at point c
+    /// @return the 2x2 tensor II according to Rusinkiewicz's formula.
+    static
+    RealTensor2D secondFundamentalForm
+    ( const RealPoint& a, const RealPoint& b, const RealPoint& c,
+      const RealVector& ua, const RealVector& ub, const RealVector& uc )
+    {
+      LSMatrix M;
+      LSColumnVector Y;
+      prepareFit( M, Y, a, b, c, ua, ub, uc );
+      RealTensor2D II;
+      bool ok = computeFit( II, M, Y );
+      return II;
+    }    
+
+    /// Computes the mean curvature on triangle abc
+    /// given normal vectors \a ua, \a \ub, \a uc at its vertices.
+    ///
+    /// @param a any point
+    /// @param b any point
+    /// @param c any point
+    /// @param ua the corrected normal vector at point a
+    /// @param ub the corrected normal vector at point b
+    /// @param uc the corrected normal vector at point c
+    /// @return the mean curvature according to Rusinkiewicz's formula.
+    static
+    Scalar meanCurvature
+    ( const RealPoint& a, const RealPoint& b, const RealPoint& c,
+      const RealVector& ua, const RealVector& ub, const RealVector& uc )
+    {
+      const auto II = secondFundamentalForm( a, b, c, ua, ub, uc );
+      return 0.5 * ( II(0,0)+II(1,1) );
+    }
+
+    /// Computes the Gaussian curvature on triangle abc
+    /// given normal vectors \a ua, \a \ub, \a uc at its vertices.
+    ///
+    /// @param a any point
+    /// @param b any point
+    /// @param c any point
+    /// @param ua the corrected normal vector at point a
+    /// @param ub the corrected normal vector at point b
+    /// @param uc the corrected normal vector at point c
+    /// @return the Gaussian curvature according to Rusinkiewicz's formula.
+    static
+    Scalar gaussianCurvature
+    ( const RealPoint& a, const RealPoint& b, const RealPoint& c,
+      const RealVector& ua, const RealVector& ub, const RealVector& uc )
+    {
+      const auto II = secondFundamentalForm( a, b, c, ua, ub, uc );
+      return II(0,0) * II(1,1) - II(0,1) * II(1,0);
+    }
+
+    /// Computes second fundamental form of polygonal face \a x given 
+    ///  normal vectors at vertices \a u.
+    /// @param x the (ccw ordered) points forming the vertices of a polygonal face.
+    /// @param u the (ccw ordered) normal vectors at the corresponding vertices in \a x.
+    ///
+    /// @return the second fundamental form of polygonal face \a x,
+    /// expressed in the basis of the triangle (b,x[0],x[1]), or
+    /// (x[0], x[1], x[2]) if it is a triangle.
+    static
+    RealTensor2D secondFundamentalForm( const RealPoints& x, const RealVectors& u )
+    {
+      ASSERT( x.size() == u.size() );
+      if ( x.size() <  3 ) return RealTensor2D();
+      if ( x.size() == 3 )
+	return secondFundamentalForm( x[ 0 ], x[ 1 ], x[ 2 ],
+				      u[ 0 ], u[ 1 ], u[ 2 ] );
+      const RealPoint   b = barycenter( x );
+      const RealVector ub = averageUnitVector( u );
+
+      const auto   basisp = basis( b, x[ 0 ], x[ 1 ] );
+      const auto       up = basisp.first;
+      const auto       vp = basisp.second;
+      RealTensor2D     II;
+      Scalar           w = 0.0;
+      for ( Index i = 0; i < x.size(); i++ )
+	{
+	  const auto basisf = basis( b, x[ i ], x[ (i+1)%x.size() ] );
+	  const auto II_i = secondFundamentalForm( b, x[ i ], x[ (i+1)%x.size() ],
+						   ub,u[ i ], u[ (i+1)%x.size() ] );
+	  const auto w_i  = area( b, x[ i ], x[ (i+1)%x.size() ] );
+	  w  += w_i;
+	  II += w_i * transformTensor2D( II_i, basisf.first, basisf.second, up, vp );
+	}
+      return (1.0 / w) * II;
+    }
+
+    /// Computes mean curvature of polygonal face \a pts given normal
+    /// vectors at vertices \a u.
+    /// @param pts the (ccw ordered) points forming the vertices of a polygonal face.
+    /// @param u the (ccw ordered) normal vectors at the corresponding vertices in \a pts.
+    /// @return the mean curvature of the given polygonal face.
+    static
+    Scalar meanCurvature( const RealPoints& pts, const RealVectors& u )
+    {
+      const auto II = secondFundamentalForm( pts, u );
+      return 0.5 * ( II(0,0)+II(1,1) );
+    }
+
+    /// Computes Gaussian curvature of polygonal face \a pts given normal
+    /// vectors at vertices \a u.
+    /// @param pts the (ccw ordered) points forming the vertices of a polygonal face.
+    /// @param u the (ccw ordered) normal vectors at the corresponding vertices in \a pts.
+    /// @return the Gaussian curvature of the given polygonal face.
+    static
+    Scalar gaussianCurvature( const RealPoints& pts, const RealVectors& u )
+    {
+      const auto II = secondFundamentalForm( pts, u );
+      return II(0,0) * II(1,1) - II(0,1) * II(1,0);
+    }
+
+    /// @}
+
+    //-------------------------------------------------------------------------
+  public:
+    /// @name Least squares services
+    /// @{
+
+    static
+    bool computeFit( RealTensor2D& II, const LSMatrix& M, const LSColumnVector& Y )
+    {
+      const auto  tM = M.transpose(); // M(6x3), tM(3*6)
+      const auto tMM = tM * M;        // tMM(3x3)
+      if ( tMM.determinant() != 0 ) {
+	const auto R = tMM.inverse() * ( tM * Y );
+	II.setComponent( 0, 0, R[ 0 ] );
+	II.setComponent( 0, 1, R[ 1 ] );
+	II.setComponent( 1, 0, R[ 1 ] );
+	II.setComponent( 1, 1, R[ 2 ] );
+	return true;
+      }
+      return false;
+    }
+    
+    static
+    void prepareFit( LSMatrix& M, LSColumnVector& Y,
+		     const RealPoint& a, const RealPoint& b, const RealPoint& c,
+		     const RealVector& ua, const RealVector& ub, const RealVector& uc )
+    {
+      std::array< RealVector, 3 >  e = { c - b, a - c, b - a };
+      std::array< RealVector, 3 > dn = { uc - ub, ua - uc, ub - ua };
+      const auto bf = basis( a, b, c );
+      const auto u  = bf.first;
+      const auto v  = bf.second;
+      for ( Dimension i = 0; i < 3; ++i )
+	{
+	  Y[ 2*i   ] = dn[ i ].dot( u );
+	  Y[ 2*i+1 ] = dn[ i ].dot( v );
+	  M.setComponent( 2*i, 0, e[ i ].dot( u ) );
+	  M.setComponent( 2*i, 1, e[ i ].dot( v ) );
+	  M.setComponent( 2*i, 2, 0.0 );
+	  M.setComponent( 2*i+1, 0, 0.0 );
+	  M.setComponent( 2*i+1, 1, e[ i ].dot( u ) );
+	  M.setComponent( 2*i+1, 2, e[ i ].dot( v ) );
+	}
+    }
+    
+    //-------------------------------------------------------------------------
+  public:
+    /// @name Other geometric services
+    /// @{
+    
+    /// Given a vector of points, returns its barycenter.
+    /// @param pts any vector of points
+    /// @return the barycenter of these points.
+    static 
+    RealPoint barycenter( const RealPoints& pts )
+    {
+      RealPoint b;
+      for ( auto p : pts ) b += p;
+      b /= pts.size();
+      return b;
+    }
+
+    /// Computes a unit normal vector to triangle abc
+    ///
+    /// @param a any point
+    /// @param b any point
+    /// @param c any point
+    /// @return the unit normal vector to abc, ( ab x ac ) / || ab x ac ||.
+    static
+    RealVector normal( const RealPoint& a, const RealPoint& b, const RealPoint& c )
+    {
+      return ( ( b - a ).crossProduct( c - a ) ).getNormalized();
+    }    
+
+    /// Computes triangle area
+    /// @param a any point
+    /// @param b any point
+    /// @param c any point
+    /// @return the area of triangle abc
+    static
+    Scalar area( const RealPoint& a, const RealPoint& b, const RealPoint& c )
+    {
+      return 0.5 * ( ( b - a ).crossProduct( c - a ) ).norm();
+    }    
+
+    /// Given a vector of unit vectors, returns their average unit vector.
+    /// @param pts any vector of vectors.
+    /// @return the average unit vector.
+    static 
+    RealVector averageUnitVector( const RealVectors& vecs )
+    {
+      RealVector avg;
+      for ( auto v : vecs ) avg += v;
+      auto avg_norm = avg.norm();
+      return avg_norm != 0.0 ? avg / avg_norm : avg;
+    }
+    
+    /// Transforms tensor \a T from frame (uf,vf) to frame (up,vp).
+    static 
+    RealTensor2D transformTensor2D( const RealTensor2D& T,
+				    const RealVector& uf, const RealVector& vf, 
+				    const RealVector& up, const RealVector& vp )
+    {
+      RealTensor2D U;
+      const auto up_uf = up.dot( uf );
+      const auto up_vf = up.dot( vf );
+      const auto vp_uf = vp.dot( uf );
+      const auto vp_vf = vp.dot( vf );
+      U.setComponent
+	( 0, 0, RowVector2D{ up_uf, up_vf }.dot(T * ColumnVector2D{ up_uf, up_vf }) );
+      U.setComponent
+	( 0, 1, RowVector2D{ up_uf, up_vf }.dot(T * ColumnVector2D{ vp_uf, vp_vf }) );
+      U.setComponent
+	( 1, 0, RowVector2D{ vp_uf, vp_vf }.dot(T * ColumnVector2D{ up_uf, up_vf }) );
+      U.setComponent
+	( 1, 1, RowVector2D{ vp_uf, vp_vf }.dot(T * ColumnVector2D{ vp_uf, vp_vf }) );
+      return U;
+    }
+    
+    /// @}
+    
+  };
+
+} // namespace DGtal
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Includes inline functions.
+//#include "RusinkiewiczCurvatureFormula.ih"
+
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+#endif // !defined RusinkiewiczCurvatureFormula_h
+
+#undef RusinkiewiczCurvatureFormula_RECURSES
+#endif // else defined(RusinkiewiczCurvatureFormula_RECURSES)
--- simpleTest.cpp
+++ simpleTest.cpp
@@ -8,6 +8,16 @@
 #include "polyscope/surface_mesh.h"
 
 
+#include "geometrycentral/surface/halfedge_mesh.h"
+#include "geometrycentral/surface/meshio.h"
+#include "geometrycentral/surface/vertex_position_geometry.h"
+
+#include "args/args.hxx"
+
+#include "CorrectedNormalCurrentFormula.h"
+#include "NormalCycleFormula.h"
+#include "RusinkiewiczCurvatureFormula.h"
+
 using namespace DGtal;
 using namespace Z3i;
 
@@ -15,56 +25,166 @@ using namespace Z3i;
 typedef Shortcuts<Z3i::KSpace>         SH3;
 typedef ShortcutsGeometry<Z3i::KSpace> SHG3;
 
-int main()
+
+using namespace geometrycentral;
+using namespace geometrycentral::surface;
+
+// == Geometry-central data
+std::unique_ptr<HalfedgeMesh> mesh;
+std::unique_ptr<VertexPositionGeometry> geometry;
+
+// Polyscope visualization handle, to quickly add data to the surface
+polyscope::SurfaceMesh *psMesh;
+
+float clampM=10.0;
+
+// Example computation function -- this one computes and registers a scalar
+// quantity
+void doWork()
 {
+  geometry->requireVertexGaussianCurvatures();
+  geometry->requireFaceNormals();
+  geometry->requireVertexNormals();
+  geometry->requireVertexPositions();
+  psMesh->addVertexScalarQuantity("curvature polyscope",
+                                  geometry->vertexGaussianCurvatures,
+                                  polyscope::DataType::SYMMETRIC);
+  
+  FaceData<double> cncMean(*mesh);
+  FaceData<double> cncGauss(*mesh);
+  FaceData<double> rusMean(*mesh);
+  FaceData<double> rusGauss(*mesh);
+  FaceData<double> m0(*mesh);
+  FaceData<double> m1(*mesh);
+  FaceData<double> m2(*mesh);
+  VertexData<RealPoint> normV(*mesh);
+
+  VertexData<double> ncGauss(*mesh);
+  EdgeData<double> ncMean(*mesh);
+  
+  auto clamp= [](double v){ return (v< -clampM)? -clampM: (v>clampM)? clampM: v; };
+  
+  for(auto face: mesh->faces())
+  {
+    auto vb = face.adjacentVertices().begin();
+    auto A =  geometry->vertexPositions[*vb];
+    auto n =  geometry->vertexNormals[*vb];
+    RealPoint pA(A.x,A.y, A.z);
+    RealPoint nA(n.x,n.y, n.z);
+    normV[*vb] = nA;
+    
+    ++vb;
+    A =  geometry->vertexPositions[*vb];
+    n =  geometry->vertexNormals[*vb];
+    RealPoint pB(A.x,A.y, A.z);
+    RealPoint nB(n.x,n.y, n.z);
+    normV[*vb] = nB;
+    ++vb;
+    A =  geometry->vertexPositions[*vb];
+    n =  geometry->vertexNormals[*vb];
+    RealPoint pC(A.x,A.y, A.z);
+    RealPoint nC(n.x,n.y, n.z);
+    normV[*vb] = nC;
+    
+    m0[face] = CorrectedNormalCurrentFormula<RealPoint, RealPoint>::mu0InterpolatedU(pA, pB, pC, nA, nB, nC);
+    m1[face] = CorrectedNormalCurrentFormula<RealPoint, RealPoint>::mu1InterpolatedU(pA, pB, pC, nA, nB, nC);
+    m2[face] = CorrectedNormalCurrentFormula<RealPoint, RealPoint>::mu2InterpolatedU(pA, pB, pC, nA, nB, nC);
+    cncMean[face] = clamp(m1[face]/m0[face]);
+    cncGauss[face] = clamp(m2[face]/m0[face]);
+    
+    rusMean[face] = clamp(RusinkiewiczCurvatureFormula<RealPoint, RealPoint>::meanCurvature(pA, pB, pC, nA, nB, nC));
+    rusGauss[face] = clamp(RusinkiewiczCurvatureFormula<RealPoint, RealPoint>::gaussianCurvature(pA, pB, pC, nA, nB, nC));
+  }
+  
+  //NormalCycle per edge
+  for(auto edge: mesh->edges())
+  {
+    auto vA = edge.halfedge().vertex();
+    auto vB = edge.halfedge().twin().vertex();
+    auto A = geometry->vertexPositions[vA];
+    auto nA = geometry->faceNormal( edge.halfedge().face()  );
+    auto B = geometry->vertexPositions[vB];
+    auto nB = geometry->faceNormal(  edge.halfedge().twin().face() );
+    RealPoint pA(A.x,A.y,A.z);
+    RealPoint pB(B.x,B.y,B.z);
+    RealPoint nnA(nA.x,nA.y,nA.z);
+    RealPoint nnB(nB.x,nB.y,nB.z);
+    ncMean[ edge ] = NormalCycleFormula<RealPoint, RealPoint>::meanCurvature(pA, pB, nnA, nnB);
+  }
+  
+  //Default polyscope GC does NC
+  psMesh->addVertexScalarQuantity("NC Gauss",geometry->vertexGaussianCurvatures,
+                                polyscope::DataType::SYMMETRIC);
+  psMesh->addEdgeScalarQuantity("NC mean",ncMean,
+                                polyscope::DataType::SYMMETRIC);
+  
+  psMesh->addFaceScalarQuantity("CNC mean",cncMean,
+                                polyscope::DataType::SYMMETRIC);
+  psMesh->addFaceScalarQuantity("CNC Gauss",cncGauss,
+                                polyscope::DataType::SYMMETRIC);
+  psMesh->addFaceScalarQuantity("Rusinkiewicz mean",rusMean,
+                                polyscope::DataType::SYMMETRIC);
+  psMesh->addFaceScalarQuantity("Rusinkiewicz Gauss",rusGauss,
+                                polyscope::DataType::SYMMETRIC);
+  psMesh->addFaceScalarQuantity("mu0",m0);
+  psMesh->addFaceScalarQuantity("mu1",m1);
+  psMesh->addFaceScalarQuantity("mu2",m2);
+  psMesh->addVertexVectorQuantity("Normal at vertices", normV);
+}
+
+// A user-defined callback, for creating control panels (etc)
+// Use ImGUI commands to build whatever you want here, see
+// https://github.com/ocornut/imgui/blob/master/imgui.h
+void myCallback() {
+
+  ImGui::SliderFloat("Clamping value", &clampM, 0.0, 1000);
+  if (ImGui::Button("do work"))
+  {
+    doWork();
+  }
+}
+
+
+int main(int argc, char **argv)
+{
+  // Configure the argument parser
+   args::ArgumentParser parser("geometry-central & Polyscope example project");
+   args::Positional<std::string> inputFilename(parser, "mesh", "A mesh file.");
+
+   // Parse args
+   try {
+     parser.ParseCLI(argc, argv);
+   } catch (args::Help) {
+     std::cout << parser;
+     return 0;
+   } catch (args::ParseError e) {
+     std::cerr << e.what() << std::endl;
+     std::cerr << parser;
+     return 1;
+   }
+
+   // Make sure a mesh name was given
+   if (!inputFilename) {
+     std::cerr << "Please specify a mesh file as argument" << std::endl;
+     return EXIT_FAILURE;
+   }
+
+  
   polyscope::init();
   
-  auto params = SH3::defaultParameters() | SHG3::defaultParameters() |  SHG3::parametersGeometryEstimation();
-  
-  auto h=0.5; //gridstep
-  params( "polynomial", "goursat" )( "gridstep", h );
-  auto implicit_shape  = SH3::makeImplicitShape3D  ( params );
-  auto digitized_shape = SH3::makeDigitizedImplicitShape3D( implicit_shape, params );
-  auto K               = SH3::getKSpace( params );
-  auto binary_image    = SH3::makeBinaryImage( digitized_shape, params );
-  auto surface         = SH3::makeDigitalSurface( binary_image, K, params );
-  auto embedder        = SH3::getCellEmbedder( K );
-  SH3::Cell2Index c2i;
-  auto surfels         = SH3::getSurfelRange( surface, params );
-  auto primalSurface   = SH3::makePrimalPolygonalSurface(c2i, surface);
-  
-  //Need to convert the faces
-  std::vector<std::vector<std::size_t>> faces;
-  for(auto &face: primalSurface->allFaces())
-    faces.push_back(primalSurface->verticesAroundFace( face ));
-  
-  polyscope::registerPointCloud("Pointels", primalSurface->positions());
-  
-  auto digsurf = polyscope::registerSurfaceMesh("Primal surface", primalSurface->positions(), faces);
-  digsurf->setEdgeWidth(1.0);
-  digsurf->setEdgeColor({1.,1.,1.});
-  
-  //Computing some differential quantities
-  params("r-radius", 5.0);
-  auto normals   = SHG3::getCTrivialNormalVectors(surface, surfels, params);
-  auto normalsTrivial   = SHG3::getTrivialNormalVectors(K,surfels);
-  auto normalsII = SHG3::getIINormalVectors(binary_image, surfels, params);
-  auto Mcurv     = SHG3::getIIMeanCurvatures(binary_image, surfels, params);
-  auto Gcurv     = SHG3::getIIGaussianCurvatures(binary_image, surfels, params);
+  // Set the callback function
+  polyscope::state::userCallback = myCallback;
 
-  //Surfel area measure
-  std::vector<double> areaMeasure(surfels.size());
-  for(auto i=0; i < areaMeasure.size(); ++i)
-    areaMeasure[i] = normalsTrivial[i].dot(normalsII[i]);
-   
+  // Load mesh
+  std::tie(mesh, geometry) = loadMesh(args::get(inputFilename));
+
+  // Register the mesh with polyscope
+  psMesh = polyscope::registerSurfaceMesh(
+      polyscope::guessNiceNameFromPath(args::get(inputFilename)),
+      geometry->inputVertexPositions, mesh->getFaceVertexList(),
+      polyscopePermutations(*mesh));
+
   
-  //Attaching quantities
-  digsurf->addFaceVectorQuantity("Trivial normal vectors", normalsTrivial);
-  digsurf->addFaceVectorQuantity("CTrivial normal vectors", normals);
-  digsurf->addFaceVectorQuantity("II normal vectors", normalsII);
-  digsurf->addFaceScalarQuantity("II mean curvature", Mcurv);
-  digsurf->addFaceScalarQuantity("II Gaussian curvature", Gcurv);
-  digsurf->addFaceScalarQuantity("Surfel area measure", areaMeasure);
   
   
   polyscope::show();
