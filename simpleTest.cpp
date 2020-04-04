8f27c2c0436d5d042dce82c76fb3737dc5e4c9dc
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-04-04 14:30:26 +0200

    minor edit

--- simpleTest.cpp
+++ simpleTest.cpp
@@ -38,6 +38,30 @@ polyscope::SurfaceMesh *psMesh;
 
 float clampM=10.0;
 
+// Computing principal curvatures k1 and k2
+static
+std::pair<RealVector, RealVector>
+curvDirFromTensor(const CorrectedNormalCurrentFormula<RealVector,RealVector>::RealTensor &tensor,
+                  const double area,
+                  const RealVector &N)
+{
+  CorrectedNormalCurrentFormula<RealVector,RealVector>::RealTensor M = tensor;
+  M += M.transpose();
+  M *= 0.5;
+  const double   coef_N = 1000.0 * area;
+  // Adding 1000 area n x n to anisotropic measure
+  for ( int j = 0; j < 3; j++ )
+    for ( int k = 0; k < 3; k++ )
+      M( j, k ) += coef_N * N[ j ] * N[ k ];
+  auto V = M;
+  RealVector L;
+  std::cout<<N.norm()<<std::endl;
+  EigenDecomposition< 3, double>::getEigenDecomposition( M, V, L );
+  return std::pair<RealVector,RealVector>(V.column( 1 ),  V.column( 0 ));
+}
+
+
+
 // Example computation function -- this one computes and registers a scalar
 // quantity
 void doWork()
@@ -49,6 +73,14 @@ void doWork()
   psMesh->addVertexScalarQuantity("curvature polyscope",
                                   geometry->vertexGaussianCurvatures,
                                   polyscope::DataType::SYMMETRIC);
+  // Set vertex tangent spaces
+  geometry->requireFaceTangentBasis();
+  FaceData<Vector3> vBasisX(*mesh);
+  for(Face f : mesh->faces()) {
+    vBasisX[f] = geometry->faceTangentBasis[f][0];
+  }
+  psMesh->setFaceTangentBasisX(vBasisX);
+
   
   FaceData<double> cncMean(*mesh);
   FaceData<double> cncGauss(*mesh);
@@ -59,10 +91,21 @@ void doWork()
   FaceData<double> m2(*mesh);
   VertexData<RealPoint> normV(*mesh);
 
+  FaceData<RealVector> d1(*mesh);
+  FaceData<RealVector> d2(*mesh);
+
+  FaceData<std::complex<double>> intd1(*mesh);
+  FaceData<std::complex<double>> intd2(*mesh);
+
   VertexData<double> ncGauss(*mesh);
   EdgeData<double> ncMean(*mesh);
   
   auto clamp= [](double v){ return (v< -clampM)? -clampM: (v>clampM)? clampM: v; };
+  
+  
+  //Default polyscope GC does NC
+  for(auto vert: mesh->vertices())
+    ncGauss[vert] = clamp(geometry->vertexGaussianCurvatures[vert]);
   
   for(auto face: mesh->faces())
   {
@@ -92,9 +135,34 @@ void doWork()
     cncMean[face] = clamp(m1[face]/m0[face]);
     cncGauss[face] = clamp(m2[face]/m0[face]);
     
+    auto mxy =CorrectedNormalCurrentFormula<RealPoint, RealPoint>::muXYInterpolatedU(pA, pB, pC, nA, nB, nC);
+    RealVector dd1,dd2;
+    auto nf= geometry->faceNormal(face);
+    RealVector nFace(nf.x,nf.y,nf.z);
+    std::tie(dd1,dd2) = curvDirFromTensor(mxy, geometry->faceArea(face), nFace);
+    d1[face] = dd1;
+    d2[face] = dd2;
+    auto vB = vBasisX[face];
+    RealVector tan(vB.x,vB.y,vB.z);
+    RealVector bitan(geometry->faceTangentBasis[face][1].x,
+                     geometry->faceTangentBasis[face][1].y,
+                     geometry->faceTangentBasis[face][1].z);
+
+    double angle =std::acos(tan.dot(dd1));
+    if (bitan.dot(dd1) < 0.0)
+      angle  = 2*M_PI - angle;
+    intd1[face] = std::polar(1.0, angle);
+    
+    angle =std::acos(tan.dot(dd2));
+      if (bitan.dot(dd2) < 0.0)
+        angle  = 2*M_PI - angle;
+    intd2[face] = std::polar(1.0, angle);
+    
     rusMean[face] = clamp(RusinkiewiczCurvatureFormula<RealPoint, RealPoint>::meanCurvature(pA, pB, pC, nA, nB, nC));
     rusGauss[face] = clamp(RusinkiewiczCurvatureFormula<RealPoint, RealPoint>::gaussianCurvature(pA, pB, pC, nA, nB, nC));
   }
+  
+  
   
   //NormalCycle per edge
   for(auto edge: mesh->edges())
@@ -109,11 +177,10 @@ void doWork()
     RealPoint pB(B.x,B.y,B.z);
     RealPoint nnA(nA.x,nA.y,nA.z);
     RealPoint nnB(nB.x,nB.y,nB.z);
-    ncMean[ edge ] = NormalCycleFormula<RealPoint, RealPoint>::meanCurvature(pA, pB, nnA, nnB);
+    ncMean[ edge ] = clamp(NormalCycleFormula<RealPoint, RealPoint>::meanCurvature(pA, pB, nnA, nnB));
   }
   
-  //Default polyscope GC does NC
-  psMesh->addVertexScalarQuantity("NC Gauss",geometry->vertexGaussianCurvatures,
+  psMesh->addVertexScalarQuantity("NC Gauss",ncGauss,
                                 polyscope::DataType::SYMMETRIC);
   psMesh->addEdgeScalarQuantity("NC mean",ncMean,
                                 polyscope::DataType::SYMMETRIC);
@@ -129,6 +196,14 @@ void doWork()
   psMesh->addFaceScalarQuantity("mu0",m0);
   psMesh->addFaceScalarQuantity("mu1",m1);
   psMesh->addFaceScalarQuantity("mu2",m2);
+ 
+  psMesh->addFaceVectorQuantity("dir1 CNC",d1);
+  psMesh->addFaceVectorQuantity("dir2 CNC",d2);
+
+  psMesh->addFaceIntrinsicVectorQuantity("dir1 bis CNC",intd1);
+  psMesh->addFaceIntrinsicVectorQuantity("dir2 bis CNC",intd2);
+
+  
   psMesh->addVertexVectorQuantity("Normal at vertices", normV);
 }
 
@@ -137,7 +212,7 @@ void doWork()
 // https://github.com/ocornut/imgui/blob/master/imgui.h
 void myCallback() {
 
-  ImGui::SliderFloat("Clamping value", &clampM, 0.0, 1000);
+  ImGui::SliderFloat("Clamping value", &clampM, 0.0, 100);
   if (ImGui::Button("do work"))
   {
     doWork();
